783d8e1e4fbae2c0d9493dc7f3270038220b4848
Bernd Janzen <103428887+manjou@users.noreply.github.com>
2024-04-23 07:58:59 +0200

    Portfolio v. 2.0.2 active nav links not working scrolling up

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Hamburger Menu
+
 const navEl = document.querySelector('.nav');
 const hamburgerEl = document.querySelector('.hamburger');
 
@@ -8,4 +10,25 @@ hamburgerEl.addEventListener('click', () => {
 navEl.addEventListener('click', () => {
     navEl.classList.remove('nav--open');
     hamburgerEl.classList.remove('hamburger--open');
+});
+
+
+// Nav link active state on scroll
+const navLinkEls = document.querySelectorAll('.nav__link');
+const sectionEls = document.querySelectorAll('.section');
+
+
+window.addEventListener('scroll', () => {
+    let currentSection = 'home';
+    sectionEls.forEach(sectionEl => {
+        if (window.scrollY >= (sectionEl.offsetTop)) {
+            currentSection = sectionEl.id;
+        }
+    });
+    navLinkEls.forEach(navLinkEl => {
+        if (navLinkEl.href.includes(currentSection)) {
+            document.querySelector('.active').classList.remove('active');
+            navLinkEl.classList.add('active');
+        }
+    });
 });
